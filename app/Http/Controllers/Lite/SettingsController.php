653b05bb6eff62f088fd401e55ea2b85761fa52c
crisvito <crisvitoc@gmail.com>
2026-07-26 14:09:33 +0700

    fix: casing namespace

--- app/Http/Controllers/Lite/SettingsController.php
+++ app/Http/Controllers/Lite/SettingsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\lite;
+namespace App\Http\Controllers\Lite;
 
 use App\Http\Controllers\Controller;
 use App\Models\Advance\Management\Settings\ReceiptSetting;
