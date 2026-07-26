9b8ca1e7beac8fd568f32b658c358cdd67625ce0
crisvito <crisvitoc@gmail.com>
2026-07-26 17:24:37 +0700

    refactor: change logo size

--- resources/js/components/app/app-logo.tsx
+++ resources/js/components/app/app-logo.tsx
@@ -2,14 +2,14 @@ import { AppLogoIcon } from '@/components';
 
 export function AppLogo() {
     return (
-        <>
+        <div className="flex items-center gap-3">
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-white">
                 <AppLogoIcon className="size-5 fill-current text-[var(--primary-900)]" />
             </div>
 
-            <div className="ml-1 grid flex-1 text-left text-sm text-[var(--white)]">
-                <span className="mb-0.5 truncate leading-none font-semibold">POSAVE</span>
+            <div className="ml-1 grid flex-1 text-left text-lg text-[var(--white)]">
+                <span className="truncate leading-none font-bold">POSAVE</span>
             </div>
-        </>
+        </div>
     );
 }
